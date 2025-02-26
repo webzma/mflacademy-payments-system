3d6f964fb3d08527f6432e515cb003fbb7794be5
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2025-02-26 00:39:32 -0400

    Refactor Sheet component for improved readability and add background color to Sidebar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -206,7 +206,9 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            <div className="flex h-full w-full flex-col">{children}</div>
+            <div className="flex h-full w-full flex-col bg-white">
+              {children}
+            </div>
           </SheetContent>
         </Sheet>
       );
