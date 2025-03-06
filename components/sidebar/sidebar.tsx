0d5a996976f35c780954b5f6e5d0aa058fc3ac1e
emcahell <emmanuelltowers@gmail.com>
2025-03-05 20:38:04 -0400

    correciones en el diseño del  navbar

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -72,8 +72,8 @@ export function AppSidebar({ userType }: SidebarProps) {
                   href={item.href}
                   className={`flex items-center space-x-3 px-6 py-3 text-md font-medium transition-colors rounded-lg ${
                     pathname === item.href
-                      ? "text-blue-600 dark:text-blue-500 bg-gray-100 hover:bg-slate-100 dark:bg-card hover:text-blue-600 dark:hover:text-blue-600 "
-                      : "text-muted-foreground hover:text-blue-600 dark:hover:text-blue-500  hover:bg-gray-100"
+                      ? "text-blue-600 dark:text-blue-500 bg-gray-100 hover:bg-slate-100 dark:bg-card hover:text-blue-500 dark:hover:text-blue-600 "
+                      : "text-muted-foreground hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100"
                   }`}
                 >
                   <item.icon className="h-5 w-5" />
@@ -96,7 +96,7 @@ export function AppSidebar({ userType }: SidebarProps) {
             <SidebarMenuButton asChild>
               <Link
                 href="/settings"
-                className="flex items-center space-x-3 px-6 py-3 text-sm font-medium text-muted-foreground hover:text-blue-600 dark:hover:text-blue-500  transition-colors rounded-lg"
+                className="flex items-center space-x-3 px-6 py-3 text-sm font-medium text-muted-foreground hover:text-blue-500 dark:hover:text-blue-500  transition-colors rounded-lg"
               >
                 <Settings className="h-5 w-5" />
                 <span>Configuración</span>
@@ -107,7 +107,7 @@ export function AppSidebar({ userType }: SidebarProps) {
             <SidebarMenuButton asChild>
               <button
                 onClick={logout}
-                className="flex items-center space-x-3 px-6 py-3 text-sm font-medium text-muted-foreground hover:text-blue-600 dark:hover:text-blue-500    transition-colors rounded-lg"
+                className="flex items-center space-x-3 px-6 py-3 text-sm font-medium text-muted-foreground hover:text-blue-500 dark:hover:text-blue-500    transition-colors rounded-lg"
               >
                 <LogOut className="h-5 w-5" />
                 <span>Cerrar Sesión</span>
